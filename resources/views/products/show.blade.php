6109b9ba165d398b0337d2bcc5b3dcc68cb1b5d7
arkawicak <arkawicak@gmail.com>
2025-04-10 23:37:12 +0700

    set product contrller

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Detail Produk</title>
+</head>
+
+<body>
+    <h1>{{ $product['name'] }}</h1>
+    <p>Harga: Rp{{ number_format($product['price'], 0, ',', '.') }}</p>
+
+    <a href="{{ url('/') }}">← Kembali ke daftar</a>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
